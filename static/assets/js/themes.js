d5e8e56711082160059127a180dbefc370612527
TY <149337748+F-SXL@users.noreply.github.com>
2026-03-10 13:23:24 +1000

    Update themes.js changed colors nd shit

--- static/assets/js/themes.js
+++ static/assets/js/themes.js
@@ -27,19 +27,10 @@ function live() {
         });
         break;
       case 'swamp':
-        applyTheme('rgba(22, 46, 23, 0.705)', "linear-gradient(to bottom, rgb(33, 116, 47), rgb(0, 0, 0))");
-        break;
-      case 'starry':
-        applyTheme('rgba(39, 81, 109, 0.521)', "linear-gradient(to right, rgb(14, 132, 211), rgb(113, 11, 209))");
-        break;
-      case 'magma':
-        applyTheme('rgb(24 24 24 / 32%)', "linear-gradient(to right, rgb(0, 0, 0), rgb(223, 59, 10))");
-        break;
-      case 'sunset':
-        applyTheme('rgb(24 24 24 / 32%)', "linear-gradient(to bottom, rgb(211, 112, 19), rgb(92, 16, 179))");
+        applyTheme('rgba(30, 30, 30, 0.705)', "linear-gradient(to bottom, rgb(33, 116, 47), rgb(0, 0, 0))");
         break;
       default:
-        applyTheme('rgba(19, 34, 48, 0.8)', "linear-gradient(to bottom right, rgb(16, 52, 82), rgb(9, 34, 55), black)");
+        applyTheme('rgba(30, 30, 30, 0.705)', "linear-gradient(to bottom right, rgb(16, 52, 82), rgb(9, 34, 55), black)");
     }
   }
   /* Ripple Effect for buttons */
